bc858d1aa2215b94f8a11e38cabbc7abcb5d6075
Dan Souza <dan.souza@tamr.com>
2020-06-01 10:53:01 -0400

    Addresses the make lint issues

--- variables.tf
+++ variables.tf
@@ -1,33 +1,33 @@
 variable "name" {
-    type = string
-    description = "Name of Databricks workspace"
+  type        = string
+  description = "Name of Databricks workspace"
 }
 variable "resource_group_name" {
-    type = string
-    description = "Name of resource group into which Databricks will be deployed"
+  type        = string
+  description = "Name of resource group into which Databricks will be deployed"
 }
 variable "location" {
-    type = string
-    description = "Location in which Databricks will be deployed"
+  type        = string
+  description = "Location in which Databricks will be deployed"
 }
 variable "virtual_network_name" {
-    type = string
-    description = "Name of existing virtual network into which Databricks will be deployed"
+  type        = string
+  description = "Name of existing virtual network into which Databricks will be deployed"
 }
 variable "virtual_network_rg_name" {
-    type = string
-    description = "Name of resource group which contiains the virtual network"
+  type        = string
+  description = "Name of resource group which contiains the virtual network"
 }
 variable "private_subnet_address_prefix" {
-    type = string
-    description = "Address space for private Databricks subnet"
+  type        = string
+  description = "Address space for private Databricks subnet"
 }
 variable "public_subnet_address_prefix" {
-    type = string
-    description = "Address space for public Databricks subnet"
+  type        = string
+  description = "Address space for public Databricks subnet"
 }
 variable "tags" {
-    type = map(string)
-    description = "Map of tags to attach to Databricks workspace"
-    default = {}
+  type        = map(string)
+  description = "Map of tags to attach to Databricks workspace"
+  default     = {}
 }
